93d2038fad70441c95a4ffa8a7bb84d278d7bce6
gregschutz <gschutz16@gmail.com>
2020-07-22 10:23:19 -0600

    added delete but requires global state like redux perhaps or useContext hook

--- BlueOwl/ClientApp/src/components/Card.js
+++ BlueOwl/ClientApp/src/components/Card.js
@@ -1,12 +1,25 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 
 export default function Card({ cards }) {
+    const [card, removeCard] = useState(cards);
+
+    const deleteCard = () => {
+        //remove card
+        //let thiscard = document.getElementsByClassName('card-text');
+        alert(card.length);
+        let c = [...card].filter((n) => parseInt(n.number) !== parseInt(num));
+        removeCard([...c]);
+
+    }
 
     const renderCard = () => {
         if (cards.visible) {
             return (
-                <div id="card" className="card mt-4 mr-4">
-                    <div className="card-header">Title</div>
+                <div id="card"  className="card mt-4 mr-4">
+                    <div className="card-header d-flex">
+                        <span className="flex-fill">Title</span>
+                        <button className="btn btn-sm btn-secondary d-inline-block justify-content-end" onClick={deleteCard}>X</button>
+                    </div>
                     <div className="card-body">
                         <h4 className="card-text text-center">{cards.number}</h4>
                     </div>
